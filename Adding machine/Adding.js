1f0a41dbfc8e0e608f4e75501b27307f326d635a
Andrew Flaugh <89938493+Flawboy@users.noreply.github.com>
2021-09-27 22:30:48 -0500

    Add files via upload

---
+++ Adding machine/Adding.js
@@ -0,0 +1,52 @@
+var x = 0;
+var y = 0;
+
+
+function val1(){
+  x = x + 1;
+  y = y + 1;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val2(){
+  x = x + 1;
+  y = y + 2;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val3(){
+  x = x + 1;
+  y = y + 3;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val4(){
+  x = x + 1;
+  y = y + 4;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val5(){
+  x = x + 1;
+  y = y + 5;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val6(){
+  x = x + 1;
+  y = y + 6;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val7(){
+  x = x + 1;
+  y = y + 7;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val8(){
+  x = x + 1;
+  y = y + 8;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function val9(){
+  x = x + 1;
+  y = y + 9;
+  document.getElementById("output").innerHTML =  "Sum: " + y + "<br>" + "Count: " + x ;
+}
+function what(){
+location.reload();
+}
